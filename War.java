957eb057575d647e943993fa1c2b94c423e5793e
HurleyJ44 <jfhurley@uvm.edu>
2014-04-30 01:42:04 -0400

    Created the War class. Started by instanciating my deck classes.

---
+++ War.java
@@ -0,0 +1,17 @@
+//Jack Hurley
+//CS110
+//4-28-14
+//Assignment 10
+//War Card Game.
+//War class
+
+import java.util.*;
+import java.random.*;
+
+public class War
+{
+   public War()
+   {
+      Deck 
+   }
+}
